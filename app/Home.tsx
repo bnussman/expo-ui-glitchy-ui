9282285429ff5709f8ed49e62f929d91fe9dfd06
Banks Nussman <banks@nussman.us>
2025-10-24 12:53:35 -0400

    clean up

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,16 +1,20 @@
 import { Button } from "@/components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { useEffect, useState } from "react";
-import { KeyboardAvoidingView, ScrollView, Text } from "react-native";
+import { ScrollView, Text } from "react-native";
 
 export function Home() {
   const navigation = useNavigation();
 
   const [show, setShow] = useState(true);
 
-  // In my real app, I update my React Query
-  // cache when the user navigates back. This just
-  // simulates that behavior...
+  /**
+   * I know this looks stupid...
+   * In my real app, my React Query cache updates
+   * and causes this component to render different contents.
+   *
+   * I'm using this to simulate that behavior...
+   */
   useEffect(() => {
     navigation.addListener("focus", (e) => {
       setShow(true);
@@ -25,16 +29,14 @@ export function Home() {
   }
 
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }}>
-      <ScrollView
-        centerContent
-        contentContainerStyle={{ padding: 8, gap: 16, alignItems: "center" }}
-      >
-        <Text>This is the "Main" page</Text>
-        <Button onPress={() => navigation.navigate("Other")}>
-          <Text>Go to other page</Text>
-        </Button>
-      </ScrollView>
-    </KeyboardAvoidingView>
+    <ScrollView
+      centerContent
+      contentContainerStyle={{ padding: 8, gap: 16, alignItems: "center" }}
+    >
+      <Text>This is the "Main" page</Text>
+      <Button onPress={() => navigation.navigate("Other")}>
+        <Text>Go to other page</Text>
+      </Button>
+    </ScrollView>
   );
 }
